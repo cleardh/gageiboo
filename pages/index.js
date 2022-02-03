8b510b290caeb6c20333bf6cf647f669fc01e50d
Dongha Kang <dongha@irisbooth.com>
2022-02-03 18:06:01 -0500

    Added darkMode toggle, Icons on navbar & started working on the form

--- pages/index.js
+++ pages/index.js
@@ -1,208 +1,62 @@
+import { useState } from 'react'
+import { Router, useRouter } from 'next/router'
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
+import '@blueprintjs/core/lib/css/blueprint.css';
+import { Intent, Spinner, Icon, IconSize, FormGroup, InputGroup, Classes } from '@blueprintjs/core'
 
 export default function Home({ isConnected }) {
+  const router = useRouter();
+  const [darkMode, setDarkMode] = useState(true);
+  const [amount, setAmount] = useState('');
+  const inputAmount = e => {
+    let lastDigit = e.target.value[e.target.value.length - 1];
+    if (typeof lastDigit === 'undefined') return setAmount('$0.00');
+    console.log((e.target.value.match(/\./g) || []).length);
+    if (
+      isNaN(lastDigit) && lastDigit !== 'Backspace' && lastDigit !== 'Enter' && lastDigit !== '.' ||
+      ((e.target.value.match(/\./g) || []).length > 1 && lastDigit === '.') ||
+      ((e.target.value.match(/\./g) || []).length > 0 && e.target.value.split('.')[1].length > 2)
+    ) return;
+    let amt = e.target.value;
+    // Need to format trailing zeroes depending on decimal points
+    setAmount(amt);
+  }
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Gageiboo</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
-
         {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
+          <>
+            <div className='navbar'>
+              <Icon icon='application' size={IconSize.LARGE} onClick={() => router.push('/chart')} className='navbar-elements' />
+              <Icon icon='grouped-bar-chart' size={IconSize.LARGE} onClick={() => router.push('/chart')} className='navbar-elements' />
+              <Icon icon='contrast' size={IconSize.LARGE} onClick={() => setDarkMode(!darkMode)} className='navbar-elements' />
+            </div>
+            <div>
+              <FormGroup
+                helperText='Helper text with details...'
+                label='Label A'
+                labelFor='text-input'
+                labelInfo='(required)'
+              >
+                <input className={`${Classes.INPUT} form-input`} type='text' placeholder='$ Amount' onChange={inputAmount} value={amount} />
+                {/* <InputGroup className='form-input' type='text' placeholder='Placeholder text' onChange={inputAmount} value={amount} />
+                <InputGroup className='form-input' type='text' placeholder='Placeholder text' onChange={inputAmount} value={amount} />
+                <InputGroup className='form-input' type='text' placeholder='Placeholder text' onChange={inputAmount} value={amount} />
+                <InputGroup className='form-input' type='text' placeholder='Placeholder text' onChange={inputAmount} value={amount} />
+                <InputGroup className='form-input' type='text' placeholder='Placeholder text' onChange={inputAmount} value={amount} /> */}
+              </FormGroup>
+            </div>
+          </>
         ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
+          <Spinner intent={Intent.WARNING} size={75} />
         )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .subtitle {
-          font-size: 2rem;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
 
       <style jsx global>{`
         html,
@@ -213,9 +67,39 @@ export default function Home({ isConnected }) {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
+        }
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background: ${darkMode ? '#30404d' : '#f5f5f5'};
+          color: ${darkMode ? '#f5f5f5' : '#30404d'};
+        }
+        .navbar {
+          width: 100%;
+          height: 40px;
+          position: fixed;
+          left: 0;
+          top: 0;
+          background: #188050;
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          padding-right: 100px;
+          color: #f5f5f5;
+        }
+        .navbar-elements {
+          cursor: pointer;
+          margin: 0 15px;
+          ${!darkMode && 'filter: invert(1);'}
+        }
+        .form-input {
+          margin: 10px 0;
         }
       `}</style>
     </div>
@@ -226,10 +110,13 @@ export async function getServerSideProps(context) {
   try {
     // client.db() will be the default database passed in the MONGODB_URI
     // You can change the database by calling the client.db() function and specifying a database like:
-    // const db = client.db("myDatabase");
+    // const db = client.db('myDatabase');
     // Then you can execute queries against your database like so:
     // db.find({}) or any of the MongoDB Node Driver commands
-    await clientPromise
+    const client = await clientPromise
+    // const db = client.db('gageiboo')
+    // const transactions = await db.collection('transactions').find({}).toArray()
+    // console.log(transactions[0].amount)
     return {
       props: { isConnected: true },
     }

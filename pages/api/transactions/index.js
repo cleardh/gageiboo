f290073f204ef9afc127e36b47efeeaecd574b17
Dongha Kang <dongha@irisbooth.com>
2022-06-08 15:06:36 -0400

    Added update api route

--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import clientPromise from '../../../lib/mongodb';
 const collection = process.env.DB_ENV === 'staging' ? 'transactions-staging' : 'transactions';
 
@@ -17,6 +18,25 @@ export default async function handler(req, res) {
             try {
                 const transactions = await db.collection(collection).find({}).toArray();
                 res.send(transactions);
+            } catch (err) {
+                console.log(err);
+            }
+            break;
+        case 'PUT':
+            try {
+                const _id = ObjectId(req.body._id);
+                delete req.body._id;
+                await db.collection(collection).replaceOne({ _id }, req.body);
+                res.send('Done');
+            } catch (err) {
+                console.log(err);
+            }
+            break;
+        case 'DELETE':
+            try {
+                const _id = ObjectId(req.body._id);
+                await db.collection(collection).deleteOne({ _id });
+                res.send('Done');
             } catch (err) {
                 console.log(err);
             }

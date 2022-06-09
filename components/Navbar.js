ee2644c1fd5d865955a1bda43295c6726254d9da
Dongha Kang <dongha@irisbooth.com>
2022-06-09 15:13:28 -0400

    Added db backup function and endpoint to download backup file

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,6 +9,7 @@ const GlobalNavbar = ({ toggleDarkMode }) => {
                 <Link href='/'><Icon icon='insert' size={IconSize.LARGE} className='navbar-elements' /></Link>
                 <Link href='/chart'><Icon icon='chart' size={IconSize.LARGE} className='navbar-elements' /></Link>
                 <Link href='/raw'><Icon icon='th' size={IconSize.LARGE} className='navbar-elements' /></Link>
+                <Link href='/api/download' passHref><a style={{ color: '#f5f5f5' }} target='_blank'><Icon icon='download' size={IconSize.LARGE} className='navbar-elements' /></a></Link>
                 <Icon icon='contrast' size={IconSize.LARGE} onClick={toggleDarkMode} className='navbar-elements' />
             </Navbar.Group>
         </Navbar>
--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -1,5 +1,8 @@
 import { ObjectId } from 'mongodb';
 import clientPromise from '../../../lib/mongodb';
+import fs from 'fs';
+import path from 'path';
+import XLSX from 'xlsx';
 const collection = process.env.DB_ENV === 'staging' ? 'transactions-staging' : 'transactions';
 
 export default async function handler(req, res) {
@@ -8,7 +11,9 @@ export default async function handler(req, res) {
     switch (req.method) {
         case 'POST':
             try {
-                await db.collection(collection).insertOne(req.body);
+                const transaction = await db.collection(collection).insertOne(req.body);
+                delete req.body._id;
+                backup({ _id: transaction.insertedId.toString(), ...req.body });
                 res.send('Done');
             } catch (err) {
                 console.log(err);
@@ -24,6 +29,7 @@ export default async function handler(req, res) {
             break;
         case 'PUT':
             try {
+                backup(req.body);
                 const _id = ObjectId(req.body._id);
                 delete req.body._id;
                 await db.collection(collection).replaceOne({ _id }, req.body);
@@ -34,6 +40,7 @@ export default async function handler(req, res) {
             break;
         case 'DELETE':
             try {
+                backup(req.body);
                 const _id = ObjectId(req.body._id);
                 await db.collection(collection).deleteOne({ _id });
                 res.send('Done');
@@ -45,4 +52,32 @@ export default async function handler(req, res) {
             break;
     }
 
+}
+
+function backup(body) {
+    const backupPath = path.join(__dirname, '../../../../gageiboo_backup.xlsx');
+    const wb = XLSX.utils.book_new();
+    wb.Props = {
+        Title: '가계부_백업',
+        Subject: '가계부_백업',
+        CreatedDate: new Date()
+    };
+    const sheetName = '가계부_백업';
+    let data = [body];
+    if (fs.existsSync(backupPath)) {
+        const existingData = getDataFromBackup(backupPath, sheetName).filter(row => row._id !== body._id);
+        data = [...existingData, body];
+        if (!body.날짜) data = existingData;
+    }
+    data.sort((a, b) => new Date(b['날짜']) - new Date(a['날짜']));
+    const ws = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(wb, ws, sheetName);
+    XLSX.writeFile(wb, backupPath);
+}
+function getDataFromBackup(backupPath, sheetName) {
+    const workbook = XLSX.readFile(backupPath, {
+        type: 'binary'
+    });
+    const XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+    return XL_row_object;
 }

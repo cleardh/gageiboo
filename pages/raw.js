427cdd5a8b105f29f035e9296d7792e46f7bee20
Dongha Kang <dongha@irisbooth.com>
2023-03-17 17:18:40 -0400

    Checking session on the api side rather than using http headers to send user email

--- pages/raw.js
+++ pages/raw.js
@@ -23,7 +23,7 @@ export default function Raw({ isConnected, user }) {
   const getDataFromDatabase = async () => {
     try {
       setLoadingData(true);
-      const transactions = await axios.get('/api/transactions', { headers: { 'user': user.email } });
+      const transactions = await axios.get('/api/transactions');
       setLoadingData(false);
       transactions.data.sort((a, b) => new Date(b['날짜']) - new Date(a['날짜']));
       parseData(transactions.data);
